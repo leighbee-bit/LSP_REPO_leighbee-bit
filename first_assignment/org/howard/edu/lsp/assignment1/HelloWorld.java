863ca8770fcea68c2dbd4e4b0cca479d5214ef02
leighbee-bit <leighlamar.dantes@bison.howard.edu>
2026-01-17 16:30:24 -0500

    First commit!

---
+++ first_assignment/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
